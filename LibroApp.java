1eef1b4ffe270280cb93f6ffc8a49bae771c95ae
1569037 <57673130+1569037@users.noreply.github.com>
2021-07-15 12:05:04 +0200

    Update LibroApp.java

--- LibroApp.java
+++ LibroApp.java
@@ -6,13 +6,14 @@
 public class LibroApp {
 
 	public static void main(String[] args) {
-		
+		//creacion de los dos libros
 		Libro l = CrearLibros("2434029430E", "La puerta rota", "Miguel Torres", 1002);
 		Libro p = CrearLibros("3432232423", "El manillar del vecino", "Pilar Moreno", 1002);
+		//comprobacion de que libro tiene un mayor numero de paginas
 		l.compareTo(p);
 	}
 	
-	public static Libro CrearLibros(String isbn, String titulo, String autor, int nPag)
+	public static Libro CrearLibros(String isbn, String titulo, String autor, int nPag) //metodo que crea un libro y lo muestra por pantalla
 	{
 		Libro l = new Libro(isbn,titulo,autor,nPag);
 		System.out.println(l.toString());
